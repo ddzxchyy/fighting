f167dd35b6b00d420ac62f6abd70b4c09c62dfd1
ddzxchyy <945048240@qq.com>
2020-08-30 23:54:39 +0800

    aop2

--- src/main/java/cn/jzq/xqg/module/technology/spring/ioc/IocApp.java
+++ src/main/java/cn/jzq/xqg/module/technology/spring/ioc/IocApp.java
@@ -1,5 +1,11 @@
 package cn.jzq.xqg.module.technology.spring.ioc;
 
+import cn.jzq.xqg.uitl.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Inversion of Control 控制反转
  * <p>
@@ -8,5 +14,12 @@ package cn.jzq.xqg.module.technology.spring.ioc;
  * 对象无需自己创建或管理它们的依赖关系，依赖关系将
  * 自动注入到需要它们的对象中去
  */
+@RestController
+@RequestMapping("/spring/ioc")
 public class IocApp {
+
+    @GetMapping("/user/{userId}")
+    public R getUserInfo(@PathVariable("userId") Integer userId) {
+       return R.ok();
+    }
 }
---
+++ src/main/java/cn/jzq/xqg/module/technology/spring/ioc/RoleEntity.java
@@ -0,0 +1,8 @@
+package cn.jzq.xqg.module.technology.spring.ioc;
+
+public class RoleEntity {
+
+    private String id;
+
+    private String name;
+}
